dadfbfe78362404311db139d2f722fcb974ca802
Jaakko <jaakko.ropponen98@gmail.com>
2020-11-11 19:31:54 +0200

    Changed to Java in android files

---
+++ flutter_app/security_control/android/app/src/main/java/com/oamkprojects/security_control/MainActivity.java
@@ -0,0 +1,6 @@
+package com.oamkprojects.security_control;
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity  extends FlutterActivity {
+
+}
--- flutter_app/security_control/android/app/src/main/kotlin/com/oamkprojects/security_control/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.oamkprojects.security_control
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
