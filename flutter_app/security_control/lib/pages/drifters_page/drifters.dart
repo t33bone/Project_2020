df3e1eed02fbe240cccf6891466167ca5462a85b
t8haja03 <56733444+t8haja03@users.noreply.github.com>
2020-11-16 03:21:10 +0200

    Added Maps
    Maps section draft added with markers

--- flutter_app/security_control/lib/pages/drifters_page/drifters.dart
+++ flutter_app/security_control/lib/pages/drifters_page/drifters.dart
@@ -1,4 +1,6 @@
+import 'dart:async'; //maps
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart'; //maps
 
 //TODO: conform to servers model of GoPiGo
 class GoPiGo {
@@ -16,6 +18,38 @@ class _DriftersPageState extends State<DriftersPage> {
   //TODO: get rid of static test names
   //final _name = <String>['test1', 'pekka', 'turpeinen'];
   final gopigolist = <GoPiGo>[];
+
+  //maps stuff
+  //TODO: replace test markers
+  final Set<Marker> _checkpoints = {
+    Marker(
+        markerId: MarkerId("gopigo_chkp_1"),
+        position: LatLng(65.060803, 25.469838),
+        infoWindow: InfoWindow(title: 'checkpoint 1')),
+    Marker(
+        markerId: MarkerId("gopigo_chkp_2"),
+        position: LatLng(65.061405, 25.464516),
+        infoWindow: InfoWindow(title: 'checkpoint 2')),
+    Marker(
+        markerId: MarkerId("gopigo_chkp_3"),
+        position: LatLng(65.059292, 25.467799),
+        infoWindow: InfoWindow(title: 'checkpoint 3')),
+    Marker(
+        markerId: MarkerId("gopigo_chkp_4"),
+        position: LatLng(65.058998, 25.463282),
+        infoWindow: InfoWindow(title: 'checkpoint 4')),
+    /*Marker(
+        markerId: MarkerId("gopigo_car_1"),
+        position: LatLng(65.059559, 25.469848),
+        icon: BitmapDescriptor.fromAsset(
+            'lib/images/baseline_local_taxi_black_18dp.png')),*/
+  };
+  Completer<GoogleMapController> _controller = Completer();
+  static const LatLng _center = const LatLng(65.059029, 25.466243);
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,23 +70,65 @@ class _DriftersPageState extends State<DriftersPage> {
   Widget _mapSection() {
     return Card(
       clipBehavior: Clip.antiAlias,
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: Center(
-                    child: Text('Map Section goes Here',
-                        style: Theme.of(context).textTheme.headline6),
-                  ),
-                )
-              ],
+      child: SizedBox(
+        //TODO make this dynamic size for bigger/smaller screens
+        height: 300.0,
+        width: 350.0,
+        child: Stack(
+          children: [
+            GoogleMap(
+              markers: _checkpoints,
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 15.0,
+              ),
             ),
-          )
-        ],
+            Align(
+              alignment: Alignment.topCenter,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 32.0,
+                    child: Card(
+                      //color: Theme.of(context).backgroundColor,
+                      child: Container(
+                        //padding: const EdgeInsets.all(8),
+                        child: Text('Map Section goes Here',
+                            style: Theme.of(context).textTheme.headline6),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            /*Row(
+            children: [
+              /* GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  initialCameraPosition: CameraPosition(
+                    target: _center,
+                    zoom: 11.0,
+                  ),
+                  ),*/
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      child: Center(
+                        child: Text('Map Section goes Here',
+                            style: Theme.of(context).textTheme.headline6),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),*/
+          ],
+        ),
       ),
     );
   }
@@ -61,9 +137,9 @@ class _DriftersPageState extends State<DriftersPage> {
     //TODO: get rid of static test names & GoPiGo
     final List<String> _nametest = <String>['te321', 'p444ka', 'thjn'];
     final List<GoPiGo> gopigolisttest = <GoPiGo>[
-      GoPiGo('te321', 23),
-      GoPiGo('p444ka', 46),
-      GoPiGo('thjn', 75)
+      GoPiGo('test car1', 23),
+      GoPiGo('BoB', 46),
+      GoPiGo('gopigo5', 75)
     ];
     return Card(
       clipBehavior: Clip.antiAlias,
@@ -76,7 +152,13 @@ class _DriftersPageState extends State<DriftersPage> {
               'GoPiGo Section Here',
               style: Theme.of(context).textTheme.headline6,
             ),
-            for (var i in gopigolisttest) _goPiGotestListTile(i),
+            for (var i in gopigolisttest)
+              Column(
+                children: [
+                  Divider(),
+                  _goPiGotestListTile(i),
+                ],
+              ),
           ],
         ),
       ),
