dfb3adc89c63a7982f2f59427a6b277d836aed5e
t8toel00 <57091384+t8toel00@users.noreply.github.com>
2020-12-02 17:35:49 +0200

    Added some RestApi methods

--- server/API/RestApi.py
+++ server/API/RestApi.py
@@ -76,6 +76,135 @@ def postStatus():
 
     return "Post successful"
 
+##################################################
+############## Android app start #################
+
+# Messages
+# Get messages where active is set to 1 (which is a default value upon creating a new Message)
+@app.route('/api/devices/get/activemessages', methods=['GET'])
+def getActiveMessages():
+    print (request.is_json)
+    content = request.get_json()
+    print(content)
+    query = '''SELECT Message.idMessage, Message.Messagetype, Message.Explanation, Message.Timestamp, Devices.Devicename
+    FROM Devices INNER JOIN Message ON Devices.idDevice = Message.Devices_idDevice WHERE Message.Active = "1"'''
+    data = db.sqlQuery(query)
+    return jsonify(data)
+
+# Post update to set message to 0 (inactive)
+@app.route('/api/message/post/messageinactive', methods=['POST'])
+def postInactive():
+    print (request.is_json)
+    content = request.get_json()
+    print(content)
+    query = '''UPDATE Message SET Active = "0" 
+               WHERE idMessage = {}'''.format(content['idMessage']) # TODO: check if this works. Update instead of Insert
+    db.sqlInsert(query)
+    return "Post successful"
+
+# Sensors
+# Get all ruuvitag id's
+@app.route('/api/devices/get/ruuvitagid', methods=['GET'])
+def getRuuvitagID():
+    print (request.is_json)
+    content = request.get_json()
+    print(content)
+    query = '''SELECT DISTINCT idDevice FROM Devices WHERE DeviceType = "ruuvitag"'''
+    data = db.sqlQuery(query)
+    return jsonify(data)
+
+# Get latest details of ruuvitag by id
+@app.route('/api/door/get/<variable>', methods=['GET']) # TODO: find a way to GET by device ID as this might not work
+def getRuuvitagLatest(variable):                        # -> Get with a variable
+    print (request.is_json)
+    content = request.get_json()
+    print(content)
+    query = '''SELECT Battery.BatteryStatus, Devices.DeviceName, Location.Segment, Door_status.OpenOrNot, 
+                Measurements.Temperature, Measurements.Humidity, Measurements.AirPressure, Devices.Connected
+                FROM Door LEFT JOIN Devices ON Door.Devices_idDevice = Devices.idDevice 
+                LEFT JOIN Door_status ON Door.idDoor = Door_status.Door_idDoor
+                LEFT JOIN Measurements ON Devices.idDevice = Measurements.Devices_idDevice
+                LEFT JOIN Battery ON Devices.idDevice = Battery.Devices_idDevice 
+                LEFT JOIN Location ON Devices.idDevice = Location.Devices_idDevice 
+                WHERE Devices.idDevice = "{}" ORDER BY Measurements.Timestamp DESC'''.format(variable)
+    data = db.sqlQuery(query)
+    return jsonify(data)
+
+# Get limits of ruuvitag by id
+@app.route('/api/devices/get/<variable>', methods=['GET'])
+def getRuuvitagLimits(variable):
+    print (request.is_json)
+    content = request.get_json()
+    print(content)
+    query = '''SELECT Measurements_Limits.Temperature, Measurements_Limits.Humidity, Measurements_Limits.AirPressure, 
+                Measurements_Limits.Batterylimit FROM Devices LEFT JOIN Measurements_Limits 
+                ON Measurements_Limits.Devices_idDevice = Devices.idDevice WHERE Devices.idDevice = "{}"'''.format(variable)
+    data = db.sqlQuery(query)
+    return jsonify(data)
+
+# Post ruuvitag measure limits
+@app.route('/api/message/post/messageinactive', methods=['POST'])
+def postRuuvitagLimits():
+    print (request.is_json)
+    content = request.get_json()
+    print(content)
+    query = '''UPDATE Measurements_Limits SET Temperature = "{}", Humidity = "{}", AirPressure = "{}", 
+                Batterylimit = "{}" WHERE Devices_idDevice = "{}"'''.format(
+                    content['Temperature'],
+                    content['Humidity'],
+                    content['AirPressure'],
+                    content['Batterylimit'],
+                    content['Devices_idDevice']) # TODO: check if this works. Update instead of Insert
+    db.sqlInsert(query)
+    return "Post successful"
+
+# Gopigos
+# Get Gopigo IDs
+@app.route('/api/devices/get/gopigoids', methods=['GET'])
+def getCarIDs():
+    print (request.is_json)
+    content = request.get_json()
+    print(content)
+    query = '''SELECT DISTINCT idDevice FROM Devices WHERE DeviceType = "gopigo"'''
+    data = db.sqlQuery(query)
+    return jsonify(data)
+
+# Get latest details of gopigo by ID
+@app.route('/api/devices/get/<variable>', methods=['GET'])
+def getGopigoDetails(variable):
+    print (request.is_json)
+    content = request.get_json()
+    print(content)
+    query = '''SELECT Devices.DeviceName, Battery.BatteryStatus, Location.Segment, Devices.Connected FROM Devices 
+                LEFT JOIN Battery ON Devices.idDevice = Battery.Devices_idDevice LEFT JOIN Location ON Devices.idDevice = Location.Devices_idDevice
+                WHERE idDevice = "{}" ORDER BY GREATEST(Battery.Timestamp, Location.Timestamp) LIMIT 1'''.format(variable)
+    data = db.sqlQuery(query)
+    return jsonify(data)
+
+# Charging station
+# Get current status
+@app.route('/api/devices/get/stationstatus', methods=['GET'])
+def getStationStatus():
+    print (request.is_json)
+    content = request.get_json()
+    print(content)
+    query = '''SELECT Charger_Status.Status, Devices.DeviceName FROM Devices LEFT JOIN Charger_Status 
+                ON Devices.idDevice = Charger_Status.Devices_idDevice ORDER BY Timestamp DESC LIMIT 1'''
+    data = db.sqlQuery(query)
+    return jsonify(data)
+
+# Get history of 10 older than timestamp
+@app.route('/api/devices/get/<variable>', methods=['GET'])
+def getStationHistory(variable):
+    print (request.is_json)
+    content = request.get_json()
+    print(content)
+    query = '''SELECT Devices.DeviceName, Charger_Status.Timestamp
+                FROM Devices INNER JOIN Charger_Status ON Devices.idDevice = Charger_Status.Devices_idDevice 
+                WHERE Charger_Status.Timestamp < "{}" LIMIT 10;'''.format(variable)
+    data = db.sqlQuery(query)   # 2020-12-01 12:12:12 (yyyy-mm-dd hh:mm:ss) timestamp format as input
+    return jsonify(data)
+
 #
 # End
 
--- server/API/appTest_get_restclient.py
+++ server/API/appTest_get_restclient.py
@@ -1,6 +1,8 @@
 
+# Scans a folder for json or txt-files, POSTs them, and moves them to an archive folder
+# Other files, that are not intended for the DB, should not be kept at the designated folders
 # Works with RestApi.py, new address routes should be added there (server side)
-# Tables are in TestDataBase in mysql (configured by RestApi.py and databaseConnect.py)
+# Tables are in mydb in mysql (configured by RestApi.py and databaseConnect.py)
 
 import requests
 import json
@@ -9,14 +11,17 @@ import time
 
 # Set a folder where program saves a file into which GET json data is stored
 saveget_directory = "D:\\OneDrive - Oulun ammattikorkeakoulu\\S2020_Projekti\\savejson\\"
-file_name = "location_status"
-file_name2 = "chargestation"
+file_name = "newest_data"
 
-getUrl = 'http://195.148.21.106/api/devices/get/devicelocationsstatus'
-getUrl2 = 'http://195.148.21.106/api/devices/get/latestcaratchargestation'
+local = False
+
+if local == False:
+    getUrl = 'http://195.148.21.106/api/doori/get/all'
+else:
+    getUrl = 'http://127.0.0.1:5000/api/doori/get/all'
 
 while(True):
-    # Gets json data from url specified above
+   # Gets json data from url specified above
     getData = requests.get(getUrl)
     # converts the data to json
     response = getData.json()
@@ -25,14 +30,4 @@ while(True):
     with open(fullpath, "w") as json_file:
         json.dump(response, json_file)
         print("Get data received and saved to: ", saveget_directory)
-
-
-    getData = requests.get(getUrl2)
-    response = getData.json()
-    
-    fullpath = os.path.join(saveget_directory, file_name2+".json")
-    with open(fullpath, "w") as json_file:
-        json.dump(response, json_file)
-        print("Get data received and saved to: ", saveget_directory)
-
     time.sleep(5)
